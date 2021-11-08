aae3f7f084f0a46b40a35212d9625956bd1c6440
JensOHI <jens@pilbro.dk>
2021-11-08 16:25:42 +0100

    changing to p5

--- main.py
+++ main.py
@@ -1,46 +1,16 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import plotly.graph_objects as go
+from p5 import setup, draw, size, background, run
+import p5
 
-from Boid import Boid
+def setup():
+    size(400,400)
 
 
-TIME_INTERVAL = 10
+def draw():
+    background(30,30,47)
 
-
-def main():
-    app = dash.Dash(__name__)
-
-    app.layout = html.Div([
-        html.H1("Boids"),
-        html.Div([
-            html.Div(id="show_time_component"),
-            dcc.Interval(
-                id="interval_component",
-                interval=TIME_INTERVAL,
-                n_intervals=0
-            )
-        ]),
-        dcc.Graph(id="graph")
-    ])
-    
-    @app.callback(
-        Output("show_time_component", "children"),
-        Output("graph", "figure"),
-        Input("interval_component", "n_intervals")
-    )
-    def update_figure(time):
-        fig = go.Figure(data=go.Cone(x=[1], y=[1], z=[1], u=[1], v=[1], w=[0]))
-        return [html.H4("Time: "+str((time*TIME_INTERVAL)/1000))], fig
-
-    app.run_server(debug=True)
-        
+    p5.fill('red')
+    p5.triangle(20,20,30,30,40,40)
 
 
 
-
-
-if __name__=="__main__":
-    main()
+run()
